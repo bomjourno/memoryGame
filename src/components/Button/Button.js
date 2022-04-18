d2f215415df66cdf5c26cdc54d6b6a2bc445aba4
Matvey Kham <mvham@mail.ru>
2022-04-18 20:13:13 +0700

    empty branch

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-function Button({handleClick, button}) {
-  return <button onClick={handleClick} type="button" className={`button button_${button}`}></button>;
-}
-
-export default Button;
